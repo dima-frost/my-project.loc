e970c8d74a4ddd9d9b2fe8e7fe45053a83ea09d0
dima-frost <frostdimonlex@gmail.com>
2019-12-24 16:27:41 +0200

    add regexp.php

---
+++ regexp.php
@@ -0,0 +1,26 @@
+<?php
+$array = [
+    'https://itea.ua',
+    'https://wikipedia.org',
+    'https://taobao.com',
+    'https://rozetka.com.ua',
+    'https://www.google.com',
+    'https://www.amazon.com',
+    'https://www.php.net',
+    'https://allegro.pl',
+    'https://telegram.org',
+    'https://www.ebay.com',
+    'https://www.mobile.de',
+    'https://prom.ua',
+    'https://aliexpress.com',
+    'https://www.ukr.net',
+];
+function searchDomain($my_array, $domain){
+    $pattern='/.\.' . $domain . '/';
+    $f_array= preg_grep($pattern, $my_array);
+    return $f_array;
+}
+
+echo '<pre>';
+print_r(searchDomain($array, "ua"));
+echo '</pre>';
